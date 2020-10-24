fd794ab590ccbbbb5935f77bf6b993d1de90fdfb
Anitesh Lal <anitesh93@gmail.com>
2020-10-23 19:24:59 -0700

    Created a FlashcardList that will display all the questions

---
+++ src/components/FlashcardList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Flashcard from './Flashcard';
+
+export default function FlashcardList() {
+  return (
+    <div>
+      <Flashcard />
+      <Flashcard />
+      <Flashcard />
+    </div>
+  );
+}
