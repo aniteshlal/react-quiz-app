fb5b72154e21e2ff2eb2b1c7506e5c30ecd103da
Anitesh Lal <anitesh93@gmail.com>
2020-10-25 01:51:42 -0700

    created a function to get all the correct answers

--- src/components/Flashcard.js
+++ src/components/Flashcard.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default function Flashcard({ flashcard }) {
+export default function Flashcard(props) {
+  const [flip, setFlip] = useState(false);
+  const { flashcard } = props;
+  const { answers, correct_answers } = flashcard;
+  const getCorrectAnswers = (
+    answersValues = Object.values(answers),
+    correctAnswersValues = Object.values(correct_answers)
+  ) => {
+    return answersValues.filter((value, index) => {
+      return correctAnswersValues[index] == 'true';
+    });
+  };
   return (
     <>
-      <div className='card'>{flashcard.question}</div>
+      <div className='card' onClick={() => setFlip(!flip)}>
+        {flip
+          ? getCorrectAnswers().map((answer) => {
+              return answer + '\n';
+            })
+          : flashcard.question}
+      </div>
     </>
   );
 }
