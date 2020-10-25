d9d45b8547191ce018faf81aa00a80c14f3eef1e
Anitesh Lal <anitesh93@gmail.com>
2020-10-25 11:43:02 -0700

    changing the html to match the new api that is formatted differently

--- src/components/Flashcard.js
+++ src/components/Flashcard.js
@@ -1,40 +1,28 @@
 import React, { useState } from 'react';
 
 export default function Flashcard(props) {
-  const [flip, setFlip] = useState(false);
   const { flashcard } = props;
-  const { answers, correct_answers } = flashcard;
-
-  const getCorrectAnswers = (
-    answersValues = Object.values(answers),
-    correctAnswersValues = Object.values(correct_answers)
-  ) => {
-    return answersValues.filter((value, index) => {
-      return correctAnswersValues[index] == 'true';
-    });
-  };
-
+  const [flip, setFlip] = useState(false);
+  console.log(flashcard.question);
   return (
-    <>
-      <div
-        className={`card ${flip ? 'flip' : ''}`}
-        onClick={() => setFlip(!flip)}
-        style={{ height: '160px' }}
-      >
-        <div className='front'>
-          {flashcard.question}
-          <div className='flashcard-options'>
-            {Object.values(answers).map((answer) => {
-              return <div className='flashcard-option'>{answer}</div>;
-            })}
-          </div>
-        </div>
-        <div className='back'>
-          {getCorrectAnswers().map((answer) => {
-            return <div className='flashcard-option-correct'>{answer}</div>;
+    <div
+      className={`card ${flip ? 'flip' : ''}`}
+      style={{ height: '150px' }}
+      onClick={() => setFlip(!flip)}
+    >
+      <div className='front'>
+        {flashcard.question}
+        <div className='flashcard-options'>
+          {flashcard.options.map((option) => {
+            return (
+              <div className='flashcard-option' key={option}>
+                {option}
+              </div>
+            );
           })}
         </div>
       </div>
-    </>
+      <div className='back'>{flashcard.answer}</div>
+    </div>
   );
 }
