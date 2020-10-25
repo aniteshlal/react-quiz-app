5c192d1db5d3a44ddfa1c5d03f0b484bc23e2a19
Anitesh Lal <anitesh93@gmail.com>
2020-10-25 12:16:22 -0700

    added the header div and also API endpoint to get all the categories available to choose from

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import FlashcardsList from './components/FlashcardList';
 import './App.css';
 import axios from 'axios';
 
 function App() {
   const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS);
+  const [categories, setCategories] = useState([]);
+
+  const categoryEl = useRef();
+
+  useEffect(() => {
+    axios.get('https://opentdb.com/api_category.php').then((res) => {
+      setCategories(res.data.trivia_categories);
+    });
+  }, []);
 
   function decodeString(str) {
     /*this function is to help format the the questions and answers properly because they contain html when returned from the api call */
@@ -32,8 +41,27 @@ function App() {
       );
     });
   }, []);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
   return (
     <>
+      <div className='header' onSubmit={handleSubmit}>
+        <div className='form-group'>
+          <label htmlFor='category'>Category</label>
+          <select id='category' ref={categoryEl}>
+            {categories.map((category) => {
+              return (
+                <option value={category.id} key={category.id}>
+                  {category.name}
+                </option>
+              );
+            })}
+          </select>
+        </div>
+      </div>
       <div className='container'>
         <FlashcardsList flashcards={flashcards} />
       </div>
