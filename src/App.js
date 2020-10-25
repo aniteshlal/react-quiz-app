a033f0cfd88ba9a3464dd36361eb32b115c723d2
Anitesh Lal <anitesh93@gmail.com>
2020-10-25 12:33:32 -0700

    actually be able to handle the submit after user selects the category and num of questions and hits submit to show the flashcards with the new questions

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { useState, useEffect, useRef } from 'react';
-import FlashcardsList from './components/FlashcardList';
+import FlashcardList from './components/FlashcardList';
 import './App.css';
 import axios from 'axios';
 
 function App() {
-  const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS);
+  const [flashcards, setFlashcards] = useState([]);
   const [categories, setCategories] = useState([]);
 
   const categoryEl = useRef();
-  const amountEl = useRef()
+  const amountEl = useRef();
 
   useEffect(() => {
     axios.get('https://opentdb.com/api_category.php').then((res) => {
@@ -23,33 +23,37 @@ function App() {
     return textArea.value;
   }
 
-  useEffect(() => {
-    axios.get('https://opentdb.com/api.php?amount=10').then((res) => {
-      setFlashcards(
-        res.data.results.map((questionItem, index) => {
-          const answer = decodeString(questionItem.correct_answer);
-          const options = [
-            ...questionItem.incorrect_answers.map((a) => decodeString(a)),
-            answer,
-          ];
-          return {
-            id: `${index}-${Date.now()}`,
-            question: decodeString(questionItem.question),
-            answer: answer,
-            options: options.sort(() => Math.random() - 0.5),
-          };
-        })
-      );
-    });
-  }, []);
-
   function handleSubmit(e) {
     e.preventDefault();
+    axios
+      .get('https://opentdb.com/api.php', {
+        params: {
+          amount: amountEl.current.value,
+          category: categoryEl.current.value,
+        },
+      })
+      .then((res) => {
+        setFlashcards(
+          res.data.results.map((questionItem, index) => {
+            const answer = decodeString(questionItem.correct_answer);
+            const options = [
+              ...questionItem.incorrect_answers.map((a) => decodeString(a)),
+              answer,
+            ];
+            return {
+              id: `${index}-${Date.now()}`,
+              question: decodeString(questionItem.question),
+              answer: answer,
+              options: options.sort(() => Math.random() - 0.5),
+            };
+          })
+        );
+      });
   }
 
   return (
     <>
-      <div className='header' onSubmit={handleSubmit}>
+      <form className='header' onSubmit={handleSubmit}>
         <div className='form-group'>
           <label htmlFor='category'>Category</label>
           <select id='category' ref={categoryEl}>
@@ -62,40 +66,26 @@ function App() {
             })}
           </select>
         </div>
-        <div className="form-group">
-          <label htmlFor="amount">Number of Questions</label>
-          <input type="number" id="amount" min="1" step="1" defaultValue={10} ref={amountEl} />
+        <div className='form-group'>
+          <label htmlFor='amount'>Number of Questions</label>
+          <input
+            type='number'
+            id='amount'
+            min='1'
+            step='1'
+            defaultValue={10}
+            ref={amountEl}
+          />
         </div>
-        <div className="form-group">
-          <button className="btn">Generate</button>
+        <div className='form-group'>
+          <button className='btn'>Generate</button>
         </div>
-      </div>
+      </form>
       <div className='container'>
-        <FlashcardsList flashcards={flashcards} />
+        <FlashcardList flashcards={flashcards} />
       </div>
     </>
   );
 }
-
-const SAMPLE_FLASHCARDS = [
-  {
-    id: 1,
-    question:
-      'Which of these songs by Skrillex features Fatman Scoop as a side artist?',
-    answer: 'Recess',
-    options: [
-      'Recess',
-      'All is Fair in Love and Brostep',
-      'Rock N Roll (Will Take You to the Mountain)',
-      'Scary Monsters and Nice Sprites',
-    ],
-  },
-  {
-    id: 2,
-    question: 'Which animation studio animated &quot;Psycho Pass&quot;?',
-    answer: 'Production I.G',
-    options: ['Kyoto Animation', 'Shaft', 'Trigger', 'Production I.G'],
-  },
-];
 
 export default App;
